6bc36ff92fecf84315e483dfdb9ffcbb065b1747
shivam <koshyarishivam43@gmail.com>
2026-01-08 23:09:28 +0530

    fixed cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,9 @@ import retryRoutes from "./routes/retry.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 app.use(express.json());
 
 app.use("/api/events", eventsRoutes);
